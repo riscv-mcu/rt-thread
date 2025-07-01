e281a14756908eb7e3c028d710b3abcc0e1312c3
Huaqi Fang <578567190@qq.com>
2025-07-01 18:18:11 +0800

    bsp/nuclei: only use ddr mode when smp enabled

--- bsp/nuclei/nuclei_fpga_eval/rtconfig.py
+++ bsp/nuclei/nuclei_fpga_eval/rtconfig.py
@@ -33,10 +33,17 @@ BUILD = 'debug'
 NUCLEI_SDK_SOC = "evalsoc"
 NUCLEI_SDK_BOARD = "nuclei_fpga_eval"
 # Configurable options below
-# DOWNLOAD: https://doc.nucleisys.com/nuclei_sdk/develop/buildsystem.html#download
-NUCLEI_SDK_DOWNLOAD = "ddr"
 # CORE: See https://doc.nucleisys.com/nuclei_sdk/develop/buildsystem.html#core
-NUCLEI_SDK_CORE = "n900fd"
+NUCLEI_SDK_CORE = "nx900fd"
+NUCLEI_SDK_SMP = 4
+
+# DOWNLOAD: https://doc.nucleisys.com/nuclei_sdk/develop/buildsystem.html#download
+if NUCLEI_SDK_SMP > 1:
+    # when run with SMP, ddr mode or sram mode is required
+    NUCLEI_SDK_DOWNLOAD = "ddr"
+else:
+    NUCLEI_SDK_DOWNLOAD = "ilm"
+
 
 if PLATFORM == 'gcc':
     # toolchain settings
@@ -57,10 +64,14 @@ if PLATFORM == 'gcc':
     OBJDUMP = PREFIX + 'objdump'
     OBJCPY  = PREFIX + 'objcopy'
 
-    CFLAGS  = ' -ffunction-sections -fdata-sections -fno-common -DSMP_CPU_CNT=2'
+    CFLAGS  = ' -ffunction-sections -fdata-sections -fno-common '
+    if NUCLEI_SDK_SMP > 1:
+        CFLAGS += ' -DSMP_CPU_CNT=%s ' % (NUCLEI_SDK_SMP)
     AFLAGS  = CFLAGS
     LFLAGS  = ' --specs=nano.specs --specs=nosys.specs -nostartfiles -Wl,--gc-sections '
-    LFLAGS += ' -Wl,-cref,-Map=rtthread.map -Wl,--defsym=__SMP_CPU_CNT=2'
+    LFLAGS += ' -Wl,-cref,-Map=rtthread.map '
+    if NUCLEI_SDK_SMP > 1:
+        LFLAGS += ' -Wl,--defsym=__SMP_CPU_CNT=%s ' % (NUCLEI_SDK_SMP)
     LFLAGS  += ' -u _isatty -u _write -u _sbrk -u _read -u _close -u _fstat -u _lseek '
     CPATH   = ''
     LPATH   = ''
