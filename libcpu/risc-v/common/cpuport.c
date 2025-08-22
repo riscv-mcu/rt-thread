c287dfa265d332a6268c5a1238cb8ff12e5e735d
Huaqi Fang <578567190@qq.com>
2025-08-22 17:20:29 +0800

    fix(cpuport): 修正riscv port栈对齐逻辑以符合 psabi 要求

--- libcpu/risc-v/common/cpuport.c
+++ libcpu/risc-v/common/cpuport.c
@@ -42,7 +42,8 @@ rt_uint8_t *rt_hw_stack_init(void       *tentry,
     int                i;
 
     stk  = stack_addr + sizeof(rt_ubase_t);
-    stk  = (rt_uint8_t *)RT_ALIGN_DOWN((rt_ubase_t)stk, REGBYTES);
+    // align to 16 bytes for task sp to match psabi requirement
+    stk  = (rt_uint8_t *)RT_ALIGN_DOWN((rt_ubase_t)stk, 16);
     stk -= sizeof(struct rt_hw_stack_frame);
 
     frame = (struct rt_hw_stack_frame *)stk;
